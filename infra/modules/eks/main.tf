e7c82c58e9a0eb7e8ddc78300150fdfc9eb582a6
ran dubnikov <randubnikov@gmail.com>
2026-06-02 19:49:38 +0300

    add Route53 permission to EKS node group

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -24,3 +24,8 @@ module "eks" {
     Project     = "monitor"
   }
 }
+resource "aws_iam_role_policy_attachment" "node_route53" {
+  role       = module.eks.eks_managed_node_groups["default"].iam_role_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
+}
+
